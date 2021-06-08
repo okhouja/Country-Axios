2186ba09a8b7f7de94e91d78f91388685c0f1927
Omar <omar0940@gmail.com>
2021-06-08 16:25:12 +0200

    Country Axios API

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,11 +3,15 @@ import React from "react";
 const Country = ({ results }) => {
   const lands = results.map((obj, i) => {
     console.log(obj);
-    const { name, flag, capital, population } = obj;
+    const { name, flag, capital, population, timezones, languages } = obj;
+    // const languages1 = obj.languages;
+    console.log(languages);
     return (
       <div key={i}>
         <h2>{name}</h2>
         <h5>Capital: {capital}</h5>
+        <h5>languages: {languages.map((language) => language.name)}</h5>
+        <h5>timezones: {timezones}</h5>
         <h5>Population: {population}</h5>
         <img src={flag} alt={name} className="flag" />
       </div>
